3d67d24b0ebb52b9acb7e95e46fc9ae39a497eee
iranmzk <iranmpc93@gmail.com>
2022-09-11 17:54:24 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,6 +4,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BASE_URL } from '../../utils/Request';
+import { Sale } from '../../models/Sale';
 
 function SalesCard() {
 
@@ -12,11 +14,14 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(new Date());
     const [maxDate, setMaxDate] = useState(new Date());
 
+
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
-        .then(response => {
-            console.log(response.data);
-        });
+        axios.get(`${BASE_URL}/sales`)
+            .then(response => {
+                setSales(response.data.content);
+            });
     }, []);
 
     return (
@@ -55,6 +60,25 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td> R$ {sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="dsmeta-redbtm-container">
+                                            <NotificationButtom />
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            )
+                        })}
+
                         <tr>
                             <td className="show992">#341</td>
                             <td className="show576">01/09/2022</td>
@@ -62,45 +86,6 @@ function SalesCard() {
                             <td className="show992">15</td>
                             <td className="show992">11</td>
                             <td>R$5500.00</td>
-                            <td>
-                                <div className="dsmeta-redbtm-container">
-                                    <NotificationButtom />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">01/09/2022</td>
-                            <td>Maria</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$4800.00</td>
-                            <td>
-                                <div className="dsmeta-redbtm-container">
-                                    <NotificationButtom />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">01/09/2022</td>
-                            <td>Victor</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$3600.00</td>
-                            <td>
-                                <div className="dsmeta-redbtm-container">
-                                    <NotificationButtom />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">01/09/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$10000.00</td>
                             <td>
                                 <div className="dsmeta-redbtm-container">
                                     <NotificationButtom />
